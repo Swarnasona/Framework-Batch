487a7803d676dddff579b0104981053f7f5c4b43
Swarnasona <swarnap628@gmail.com>
2021-12-31 16:59:23 +0530

    Password Functionality

--- src/org/resource/Login.java
+++ src/org/resource/Login.java
@@ -1,5 +1,11 @@
 package org.resource;
 
 public class Login {
+	public static void password() {
+		System.err.println("Password");
+}
+	public static void main(String[] args) {
+		password();
+	}
 
 }
